a742226c5cce60cb9d6f62f0fc2a1612c6e12886
Chrityao <amionghislainamion888@gmail.com>
2021-10-23 14:11:40 +0000

    mon deuxieme projet

---
+++ Assets/Scripts/RacketController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketController : MonoBehaviour
+{
+
+    //Movement Speed
+    public float speed = 20;
+
+
+    void FixedUpdate()
+    {
+
+        float button = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * button * speed;
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // We have to add the UnityEngine.UI namespace in order to work with Text variables
+
+
+public class Score : MonoBehaviour
+{
+
+    public Text ScoreupText;
+    int Scoreup;
+    int Life;
+
+
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        //this is our condition (if the ball collides with an object with the name Wallright)
+        if (col.gameObject.name == "Wallright")
+        {
+            //this line will just add 1 point to the score
+            Scoreup += 10;
+            //this line will convert the int score variable to a string variable
+            ScoreupText.text = Scoreup.ToString();
+
+        }
+        //this is our condition (if the ball collides with an object with the name Walldown)
+        if (col.gameObject.name == "Walldown")
+        {
+            Scoreup -= 5;
+            ScoreupText.text = Scoreup.ToString();
+        }
+       
+    }
+}
